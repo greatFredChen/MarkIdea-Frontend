8c91e9fb99cb061c95d15ebd70b32765f575491b
LYL232 <3183246942@qq.com>
2020-05-24 04:09:26 +0800

    rebuild getDomainContains and getLocalDomain and fix return code to http code

--- cloudfunctions/getDomainContains/index.js
+++ cloudfunctions/getDomainContains/index.js
@@ -18,18 +18,21 @@ exports.main = async (event, context) => {
   // 返回
   // 正常:
   // {
-  //   code: 0 code = 0 表示正常
+  //   code: 200,
+  //   idea: idea对象列表,
+  //   relationship: 关联对象列表
   // }
   // 不正常:
   // {
-  //   code: 非0, 非0表示不正常
+  //   code: 非200
   //   error: 异常信息体
   //   msg: 异常错误信息描述
   // }
   // 参数检查
-  if (!event.domain_id || !event.backend_host) { return { code: -1, msg: '输入参数不正确', error: {} } }
+  if (!event.domain_id || !event.backend_host) { return { code: 400, msg: '输入参数不正确', error: {} } }
 
-  let response = {}
+  let idea = null
+  let relationship = null
   try {
     const res = await axios({
       url: event.backend_host + '/domain/get_domain_contains',
@@ -37,29 +40,33 @@ exports.main = async (event, context) => {
       method: 'GET',
       responseType: 'json'
     })
-    response = res.data
-    response.code = 0
+    idea = res.data.idea
+    relationship = res.data.relationship
   } catch (error) {
-    response.msg = '获取domain图结构失败'
-    response.error = error
-    if (response.error.response.data.code === 404000) {
-      response.msg += ' 原因: 找不到id为' + event.domain_id + '的domain对象'
+    return {
+      msg : '获取domain图结构失败',
+      error: error,
+      code: error.response.status ? error.response.status : 500
     }
-    response.code = -1
   }
-
-  if (response.code !== 0) {
-    // 有异常直接返回
-    return response
-  }
-  if (response.idea.length === 0) {
+  if (idea.length === 0) {
     // 如果没有id, 则直接返回
-    return response
+    return {
+      code : 200,
+      idea : [],
+      relationship: []
+    }
   }
+  
   // 查询云数据库补全idea信息
-  const ideaIdList = response.idea.map(String)
+  const ideaIdList = idea.map(String)
+  console.log(ideaIdList)
   const result = await db.collection('Idea')
     .where({ _id: cmd.in(ideaIdList) }).get()
-  response.idea = result.data
-  return response
+  console.log(result)
+  return {
+    code : 200,
+    idea : result.data,
+    relationship: relationship
+  }
 }
