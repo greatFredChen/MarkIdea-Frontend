d88e0e8444a557044881b21de5e539f4ef24ec62
LYL232 <3183246942@qq.com>
2020-05-24 02:27:33 +0800

    implements like idea component

---
+++ miniprogram/components/likeButton/likeButton.js
@@ -0,0 +1,103 @@
+// components/likeButton.js
+Component({
+
+  lifetimes: {
+    attached() {
+      // 获取用户点赞数据
+      wx.cloud.callFunction({
+        name: 'getUserLikeIdeaInfo',
+        data: {
+          idea_id: this.properties.ideaId,
+          user_id: this.properties.userId
+        }
+      }).then(res => {
+        if (res.result.code !== 200) {
+          throw result
+        }
+        this.setData({
+          liked: res.result.liked,
+          likes: res.result.likes
+        })
+      }).catch(err => {
+        wx.showToast({
+          title: '获取Idea点赞信息失败:' + err.msg,
+          icon: 'none',
+          duration: 2000
+        })
+        console.log('likeButton:获取Idea点赞信息失败')
+        console.log(err)
+      })
+    }
+  },
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    // 这个点赞按钮对应的ideaId
+    ideaId: String,
+    // 用户id
+    userId: String,
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    liked: false,
+    likes: 0,
+    enable: true // 是否有效, 防止用户短时间内多次操作出现bug
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    ontap: function () {
+      if (this.data.enable !== true) {
+        wx.showToast({
+          title: '您操作得太频繁了, 请稍后再试',
+          icon: 'none',
+          duration: 2000
+        })
+        return
+      }
+      // 先假装事务成功, 因为大概率成功.... 达到响应快的目的
+      this.setData({
+        liked: !this.data.liked,
+        likes: this.data.likes + (this.data.liked ? -1 : 1),
+        enable: false  // 组件变得不可用
+      })
+      wx.cloud.callFunction({
+        name: 'likeIdea',
+        data: {
+          idea_id: this.properties.ideaId,
+          user_id: this.properties.userId
+        }
+      }).then(res => {
+        if (res.result.code !== 200) {
+          throw result
+        }
+        // 更新数据
+        this.setData({
+          liked: res.result.liked,
+          likes: res.result.likes,
+          enable: true  // 组件重新变得可用
+        })
+        // 触发idea点赞数事件
+        this.triggerEvent('idealikeschange', {
+          idea_id: this.properties.ideaId,
+          likes: this.data.likes
+        }, {})
+      }).catch(err => {
+        wx.showToast({
+          title: '更新Idea点赞信息失败' + err.msg,
+          icon: 'none',
+          duration: 2000
+        })
+        console.log('更新Idea点赞信息失败')
+        console.log(err)
+      })
+    }
+  }
+})
